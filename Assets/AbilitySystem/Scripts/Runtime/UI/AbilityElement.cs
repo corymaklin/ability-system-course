9ec3844a35de61b2abf665713f440b389798dabf
unknown <corymaklin@gmail.com>
2021-12-25 20:37:08 -0500

    ability element

---
+++ Assets/AbilitySystem/Scripts/Runtime/UI/AbilityElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AbilitySystem.UI
+{
+    public class AbilityElement : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<AbilityElement, UxmlTraits> {}
+
+        public AbilityElement()
+        {
+            var visualTree = Resources.Load<VisualTreeAsset>("UI/Ability");
+            visualTree.CloneTree(this);
+        }
+    }
+}
